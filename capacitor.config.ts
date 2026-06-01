7178dd9042f3adc89ddf40af6ecb9b511e5cc3d9
VinsmokeSanjiiiii <rcketrcon779@gmail.com>
2026-06-01 21:55:52 +0800

    feat: android app support update

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.primer.app',
+  appName: 'Primer',
+  webDir: 'dist'
+};
+
+export default config;
